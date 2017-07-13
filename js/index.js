2b9693e6be3c0377f57fb012e8089eb823704247
Avi Aryan <avi.aryan123@gmail.com>
2017-07-13 17:07:42 +0530

    add basic map

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 // import ko
 var ko = require('knockout');
 
+// initalize vars
+var map;
+
+// load map
+initMap = function() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	});
+}
+
 // places stores the list of places one can visit in Patna
-places = [
+var places = [
 	{
 		name: 'Gol Ghar'
 	},
